1e35a36842b91d12acc05e90aa30ac47d572f4ab
sungeun1028 <sungeun1028@naver.com>
2020-12-24 18:48:51 +0900

    Webpage Section - Find cheap gas stations by region

---
+++ CODE/Step 5 - 싼 주유소 찾기 - 지역별.py
@@ -0,0 +1,5 @@
+#####################################
+# 싼 주유소 찾기 - 지역별
+#####################################
+# http://www.opinet.co.kr/searRgSelect.do -> 작동 안함, javascript 로 막음
+driver.execute_script("return goSubPage(0,0,99)")
