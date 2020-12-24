2f6dbb40d6c2f2431b2242e2f9f7b69fd0057318
sungeun1028 <sungeun1028@naver.com>
2020-12-24 18:47:19 +0900

    Install Chrome Driver

---
+++ CODE/Step 2 - Install Chrome Driver( 크롬 드라이버 설치).py
@@ -0,0 +1,21 @@
+#####################################
+# 주유쇼 관련 정보 가져오기
+#####################################
+# opinet - 유가정보 제공 | 한국석유공사
+# https://www.opinet.co.kr/
+
+
+#####################################
+# Chrome Driver 설치
+#####################################
+# Chrome 버전 확인
+# 설정 - Chrome - Chrome 정보 - Chrome 버젼 확인
+# 버전 87.0.4280.88(공식 빌드) (64비트) -> version 87
+
+# Chrome Driver 설치
+# https://chromedriver.chromium.org/downloads
+# https://chromedriver.storage.googleapis.com/index.html?path=87.0.4280.88/ (87 version 설치)
+
+# OS (Operating System, 운영체제) 확인
+# linux - chromedriver_linux64.zip	# mac - chromedriver_mac64.zip	# windows - chromedriver_win32.zip
+# Chrome Driver 설치 경로 확인 - ex. C:\Users\Sung\Downloads
